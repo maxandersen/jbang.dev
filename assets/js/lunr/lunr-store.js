564fde3bb647f5e0e8ce803b0140bdaf342b2a80
maxandersen <maxandersen@users.noreply.github.com>
2024-04-09 13:37:30 +0000

    deploy: 4d8350c3dcc7e8ca59151dcc3d33a9aa8846366c

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Beyond Hello World!",
+        "excerpt":"A simple script you can run with jbang is \"Hello World\" using jshell (.jsh). System.out.println(\"Hello \" + ((args.length&gt;0)?args[0]:\"jbang\")); You can run the above by putting it in a file named: hello.jsh and run jbang hello.jsh. To expand on this we will make a Java file (.java) which uses an external...","categories": ["learn"],
+        "tags": ["intro","beginner","jbang","tutorial"],
+        "url": "/learn/beyondhello/",
+        "teaser": "/assets/images/helloworld.jpg"
+      },{
+        "title": "JBang Avatar",
+        "excerpt":"It is getting very close to the first anniversary of first commit of JBang. To celebrate I looked around to see what we could do and was reminded about good old Gource. With Gource you can render the history of a git repository and get a nice animated video, including...","categories": ["learn"],
+        "tags": ["intro","beginner","jbang","tutorial"],
+        "url": "/learn/jbang-avatar/",
+        "teaser": "/assets/images/avatar.jpg"
+      },{
+        "title": "JEP 408: Simple Web Server with JBang",
+        "excerpt":"In March JEP: 408: Simple Web Server was announced. One of JEP 408&#8217;s goals is to make an easy to use web server available with Java using java -m jdk.httpserver. It is a nice idea and I&#8217;m looking forward to see it happen. That said - you do not need...","categories": ["learn"],
+        "tags": ["intro","beginner","jbang"],
+        "url": "/learn/jep-408-with-jbang/",
+        "teaser": "/assets/images/httpjbang.jpg"
+      },{
+        "title": "JBang Rewind 2021!",
+        "excerpt":"We just closed on a very busy 2021 for JBang, and especially in the last few weeks, things progressed quite a bit. Therefore I thought it would be nice to give a quick overview to recognize the progress and give you a chance to catch up! December 31st JDoodle updates...","categories": ["learn"],
+        "tags": ["jbang"],
+        "url": "/learn/jbang-rewind/",
+        "teaser": "/assets/images/httpjbang.jpg"
+      },{
+        "title": "jbang-npm: Java Script in your JavaScript",
+        "excerpt":"Today we are announcing jbang-npm a npm package which brings you Java Script to your JavaScript project. jbang-npm joins jbang-maven and jbang-gradle as tools that brings the power of Java Script to your favourite build setup. jbang-npm supports running any kind java code that JBang supports. Wether that is a...","categories": ["learn"],
+        "tags": ["jbang"],
+        "url": "/learn/jbang-npm/",
+        "teaser": "/assets/images/java_script.png"
+      },{
+        "title": "jbang-python: Java in your Python",
+        "excerpt":"Happy to announce jbang-python - a pypi package which brings your Java to your Python project or notebook. jbang-python supports running any kind java code that JBang supports. Wether that is a script from the AppStore, a Maven GAV, a jar or simply a java file stored in your project....","categories": ["learn"],
+        "tags": ["jbang"],
+        "url": "/learn/python-with-jbang/",
+        "teaser": "https://github.com/jbangdev/jbang-python/blob/main/python_jbang.png?raw=true"
+      },{
+        "title": "Introducing jbanghub",
+        "excerpt":"Today we are introducing JBangHub! JBang always allowed everyone easily make their own java/kotlin/groovy/etc. command line tools available in a jbang catalog. All it takes is that you make a jbang-catalog in your project, organization or website. This is what makes things like jbang jreleaser@jreleaser and jbang minecraft-server@microsoft possible with...","categories": ["learn"],
+        "tags": ["jbang"],
+        "url": "/learn/jbanghub/",
+        "teaser": "https://raw.githubusercontent.com/jbangdev/jbang.dev/main/images/jbanghublaunch.webp"
+      },{
+        "title": "JBang moving into CommonHaus Foundation",
+        "excerpt":"JBang and its associated projects is joining as a founding project of the CommonHaus foundation. CommonHaus is a brand new foundation which focus is on providing a independent home for projects like JBang that are funded by independents and with room for project indivduality. Other founding projects are Hibernate, Jackson,...","categories": ["learn"],
+        "tags": ["jbang"],
+        "url": "/learn/jbang-and-commonhaus/",
+        "teaser": "https://raw.githubusercontent.com/commonhaus/artwork/main/foundation/brand/svg/CF_icon_default.svg"
+      }]
--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-
+//JAVA 11+
 import static java.lang.System.*;
 
 import java.io.IOException;
@@ -15,9 +15,9 @@ public class main {
         out.println("It is not really how jbang.dev is best viewed, but congratulations");
         out.println("on having jbang installed.\n");
         out.println("All the best from jbang.dev and its awesome contributors!\n");
-        out.println("https://github.com/jbangdev/jbang/blob/master/CONTRIBUTORS.md\n\n");
+        out.println("https://github.com/jbangdev/jbang/blob/HEAD/CONTRIBUTORS.md\n\n");
 
-        URL u = new URL("https://raw.githubusercontent.com/jbangdev/jbang/master/CONTRIBUTORS.md");
+        URL u = new URL("https://raw.githubusercontent.com/jbangdev/jbang/HEAD/CONTRIBUTORS.md");
 
         try (Scanner scanner = new Scanner(u.openStream(), StandardCharsets.UTF_8.toString())) {
 
